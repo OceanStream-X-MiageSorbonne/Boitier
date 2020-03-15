f399f953e282cd4922e830fe0c1055802f061262
Abdel Benamara <abdelbenamara@macbook-pro-de-abdel.home>
2020-03-16 00:13:47 +0100

    lecteur ok pour la US2

--- OceanBox/src/main/java/oceanbox/controler/AbstractControler.java
+++ OceanBox/src/main/java/oceanbox/controler/AbstractControler.java
@@ -1,13 +1,39 @@
 package oceanbox.controler;
 
+import javafx.animation.PauseTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 import oceanbox.model.AbstractModel;
 
 public abstract class AbstractControler {
-	protected AbstractModel model;
 
-	public AbstractControler(AbstractModel model) {
+	protected AbstractModel model;
+	protected Stage stage;
+	protected PauseTransition closeTimer;
+	protected EventHandler<ActionEvent> end = new EventHandler<ActionEvent>() {
+		@Override
+		public void handle(ActionEvent event) {
+			stage.close();
+		}
+	};
+
+	public AbstractControler(Stage stage, AbstractModel model) {
 		this.model = model;
+		this.stage = stage;
+		this.closeTimer = new PauseTransition(Duration.seconds(20));
+		this.closeTimer.setOnFinished(end);
+		this.closeTimer.play();
 	}
 
-	abstract void control();
+	public AbstractModel getModel() {
+		return model;
+	}
+
+	public PauseTransition getCloseTimer() {
+		return closeTimer;
+	}
+
+	public abstract void control(int max);
 }
--- OceanBox/src/main/java/oceanbox/model/AbstractModel.java
+++ OceanBox/src/main/java/oceanbox/model/AbstractModel.java
@@ -5,6 +5,7 @@ import oceanbox.observer.Observer;
 import java.util.ArrayList;
 
 public abstract class AbstractModel implements Observable {
+
 	private ArrayList<Observer> listObserver = new ArrayList<Observer>();
 
 	public void addObserver(Observer obs) {
