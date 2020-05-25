17275520194c451b0e2492895592c42be7880555
da-ekchajzer <da.ekchajzer@gmail.com>
2020-05-25 15:17:11 +0200

    Motion detector implementation

--- OceanBox/src/main/java/oceanbox/veille/VeilleDMV.java
+++ OceanBox/src/main/java/oceanbox/veille/VeilleDMV.java
@@ -1,45 +1,92 @@
 package oceanbox.veille;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+
+import oceanbox.propreties.ClientPropreties;
+import oceanbox.system.Contenu;
+
 public class VeilleDMV implements Veille {
 
-	public VeilleDMV() {
+    private Contenu contenu;
+    private Timer timeBeforeVeille;
+    private Boolean sleepMode;
 
-	}
+    public VeilleDMV(Contenu c) {
+        contenu = c;
+        initMotionSensorListner();
+    }
+    
+    private void initMotionSensorListner() {
+        final GpioController gpio = GpioFactory.getInstance();
+        final GpioPinDigitalInput motionDetector = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+        motionDetector.setShutdownOptions(true);
+        motionDetector.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                if(event.getState().equals(PinState.HIGH)) {
+                    System.out.println(">>> Motion detected");
+                    update();
+                }
+            }
+        });
+    }
 
-	@Override
-	public void goInVeille() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void goInVeille() {
+        sleepMode = true;
+        contenu.stopDiffusion();
 
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void goOutVeille() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void update() {
+        if (!sleepMode && ClientPropreties.getPropertie("activateStandby").equals("true")) {
+            pushVeille();
+        } else {
+            goOutVeille();
+        }
+    }
 
-	@Override
-	public void pushVeille() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void goOutVeille() {
+        if (ClientPropreties.getPropertie("activateStandby").equals("true"))
+            initVeille();
+        sleepMode = false;
+    }
 
-	@Override
-	public void initVeille() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void pushVeille() {
+        timeBeforeVeille.cancel();
+        initVeille();
+    }
 
-	@Override
-	public boolean isSleepMode() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public void initVeille() {
+        timeBeforeVeille = new Timer();
+        timeBeforeVeille.schedule(new VeilleTask(), initMiliSecondsBeforeClose());
+    }
+
+    private long initMiliSecondsBeforeClose() {
+        String[] times = ClientPropreties.getPropertie("timeBeforeStandby").split(":");
+        return 1000 * ((Integer.parseInt(times[0]) * 3600) + (Integer.parseInt(times[1]) * 60)
+                + Integer.parseInt(times[2]));
+    }
+
+    private class VeilleTask extends TimerTask {
+        @Override
+        public void run() {
+            goInVeille();
+        }
+    }
+
+    @Override
+    public boolean isSleepMode() {
+        return sleepMode;
+    }
 
 }
