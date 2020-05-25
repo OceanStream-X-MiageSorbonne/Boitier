22114e0c515f11ea2ca414cff0e549d12c673f62
Abdel Benamara <abdelbenamara@macbook-pro-de-abdel.home>
2020-05-25 15:22:46 +0200

    correction initVeille

--- OceanBox/src/main/java/oceanbox/veille/VeilleDMV.java
+++ OceanBox/src/main/java/oceanbox/veille/VeilleDMV.java
@@ -12,81 +12,84 @@ import oceanbox.system.Contenu;
 
 public class VeilleDMV implements Veille {
 
-    private Contenu contenu;
-    private Timer timeBeforeVeille;
-    private Boolean sleepMode;
+	private Contenu contenu;
+	private Timer timeBeforeVeille;
+	private Boolean sleepMode;
 
-    public VeilleDMV(Contenu c) {
-        contenu = c;
-        initMotionSensorListner();
-    }
-    
-    private void initMotionSensorListner() {
-        final GpioController gpio = GpioFactory.getInstance();
-        final GpioPinDigitalInput motionDetector = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
-        motionDetector.setShutdownOptions(true);
-        motionDetector.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                if(event.getState().equals(PinState.HIGH)) {
-                    System.out.println(">>> Motion detected");
-                    update();
-                }
-            }
-        });
-    }
+	public VeilleDMV(Contenu c) {
+		contenu = c;
+		if (ClientPropreties.getPropertie("activateStandby").equals("1"))
+			initMotionSensorListner();
+	}
 
-    @Override
-    public void goInVeille() {
-        sleepMode = true;
-        contenu.stopDiffusion();
+	private void initMotionSensorListner() {
+		initVeille();
+		final GpioController gpio = GpioFactory.getInstance();
+		final GpioPinDigitalInput motionDetector = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02,
+				PinPullResistance.PULL_DOWN);
+		motionDetector.setShutdownOptions(true);
+		motionDetector.addListener(new GpioPinListenerDigital() {
+			@Override
+			public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+				if (event.getState().equals(PinState.HIGH)) {
+					System.out.println(">>> Motion detected");
+					update();
+				}
+			}
+		});
+	}
 
-    }
+	@Override
+	public void goInVeille() {
+		sleepMode = true;
+		contenu.stopDiffusion();
 
-    @Override
-    public void update() {
-        if (!sleepMode && ClientPropreties.getPropertie("activateStandby").equals("true")) {
-            pushVeille();
-        } else {
-            goOutVeille();
-        }
-    }
+	}
 
-    @Override
-    public void goOutVeille() {
-        if (ClientPropreties.getPropertie("activateStandby").equals("true"))
-            initVeille();
-        sleepMode = false;
-    }
+	@Override
+	public void update() {
+		if (!sleepMode && ClientPropreties.getPropertie("activateStandby").equals("1")) {
+			pushVeille();
+		} else {
+			goOutVeille();
+		}
+	}
 
-    @Override
-    public void pushVeille() {
-        timeBeforeVeille.cancel();
-        initVeille();
-    }
+	@Override
+	public void goOutVeille() {
+		if (ClientPropreties.getPropertie("activateStandby").equals("1"))
+			initVeille();
+		sleepMode = false;
+	}
 
-    @Override
-    public void initVeille() {
-        timeBeforeVeille = new Timer();
-        timeBeforeVeille.schedule(new VeilleTask(), initMiliSecondsBeforeClose());
-    }
+	@Override
+	public void pushVeille() {
+		timeBeforeVeille.cancel();
+		initVeille();
+	}
 
-    private long initMiliSecondsBeforeClose() {
-        String[] times = ClientPropreties.getPropertie("timeBeforeStandby").split(":");
-        return 1000 * ((Integer.parseInt(times[0]) * 3600) + (Integer.parseInt(times[1]) * 60)
-                + Integer.parseInt(times[2]));
-    }
+	@Override
+	public void initVeille() {
+		timeBeforeVeille = new Timer();
+		timeBeforeVeille.schedule(new VeilleTask(), initMiliSecondsBeforeClose());
+	}
 
-    private class VeilleTask extends TimerTask {
-        @Override
-        public void run() {
-            goInVeille();
-        }
-    }
+	private long initMiliSecondsBeforeClose() {
+		String[] times = ClientPropreties.getPropertie("timeBeforeStandby").split(":");
+		return 1000 * ((Integer.parseInt(times[0]) * 3600) + (Integer.parseInt(times[1]) * 60)
+				+ Integer.parseInt(times[2]));
+	}
 
-    @Override
-    public boolean isSleepMode() {
-        return sleepMode;
-    }
+	private class VeilleTask extends TimerTask {
+		@Override
+		public void run() {
+			goInVeille();
+		}
+	}
+
+	@Override
+	public boolean isSleepMode() {
+		return sleepMode;
+	}
 
 }
